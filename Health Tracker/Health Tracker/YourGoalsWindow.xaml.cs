1145c9d5a53c4d78bcdafa910e97e3c1abfd8168
anushanarayan2911 <anusha.narayan2002@outlook.com>
2021-09-17 13:05:56 +0100

    Clean up C# code for Your Goals Window

--- Health Tracker/Health Tracker/YourGoalsWindow.xaml.cs
+++ Health Tracker/Health Tracker/YourGoalsWindow.xaml.cs
@@ -22,16 +22,19 @@ namespace Health_Tracker
     /// </summary>
     public partial class YourGoalsWindow : Window
     {
+        #region StartVariables
         string GoalInfo;
         string AchieveBy;
         string Status;
         DateTime Today;
         DateTime OneWeekAway;
         Canvas StatusPopupCanvas = new Canvas();
+        #endregion
 
         public YourGoalsWindow()
         {
             InitializeComponent();
+
             Today = DateTime.Now;
             OneWeekAway = Today.AddDays(+7);
 
@@ -66,49 +69,70 @@ namespace Health_Tracker
 
         private void StatusButtonClick(object sender, RoutedEventArgs e)
         {
-            Button button = sender as Button;
-            Goal SelectedRow = button.DataContext as Goal;
+            #region GetGoal
+            Button b = sender as Button;
+            Goal SelectedRow = b.DataContext as Goal;
 
             string GoalInfo = SelectedRow.GoalInfo.ToString();
             DateTime AchieveBy = Convert.ToDateTime(SelectedRow.AchieveBy.ToString());
+            #endregion
+
+            #region StatusPopup
+
             string Status = FormatStatusPopup(AchieveBy)[0].ToString();
             StatusPopupCanvas.Background = (Brush) FormatStatusPopup(AchieveBy)[1];
 
             #region AddElementsToStatusPopup
+
+            #region GoalInfoTextBlock
             TextBlock GoalInfoTextBlock = new TextBlock();
             GoalInfoTextBlock.Text = GoalInfo;
             GoalInfoTextBlock.TextDecorations = TextDecorations.Underline;
             GoalInfoTextBlock.FontFamily = new FontFamily("Runda");
+            GoalInfoTextBlock.Uid = "GoalInfoTextBlock";
             GoalInfoTextBlock.Margin = new Thickness(150, 0, 0, 0);
             GoalInfoTextBlock.Uid = "GoalInfoTextBlock";
             StatusPopupCanvas.Children.Add(GoalInfoTextBlock);
+            #endregion
 
+            #region AchieveByLabel
             Label AchieveByLabel = new Label();
             AchieveByLabel.Content = "Achieve By: ";
             AchieveByLabel.FontFamily = new FontFamily("Runda Light");
+            AchieveByLabel.Uid = "AchieveByLabel";
             AchieveByLabel.Margin = new Thickness(0, 30, 0, 0);
             StatusPopupCanvas.Children.Add(AchieveByLabel);
+            #endregion
+
+            #region AchieveByTextBlock
 
             TextBlock AchieveByTextBlock = new TextBlock();
             AchieveByTextBlock.Text = AchieveBy.ToString().Substring(0, 10);
-            AchieveByTextBlock.Uid = "AchieveByTextBlock";
             AchieveByTextBlock.FontFamily = new FontFamily("Runda Light");
+            AchieveByTextBlock.Uid = "AchieveByTextBlock";
             AchieveByTextBlock.Margin = new Thickness(75, 35, 0, 0);
             StatusPopupCanvas.Children.Add(AchieveByTextBlock);
+            #endregion
 
+            #region StatusLabel
             Label StatusLabel = new Label();
             StatusLabel.Content = "Status";
             StatusLabel.FontFamily = new FontFamily("Runda Light");
+            StatusLabel.Uid = "StatusLabel";
             StatusLabel.Margin = new Thickness(0, 60, 0, 0);
             StatusPopupCanvas.Children.Add(StatusLabel);
+            #endregion
 
+            #region StatusTextBlock
             TextBlock StatusTextBlock = new TextBlock();
             StatusTextBlock.Text = Status;
             StatusTextBlock.FontFamily = new FontFamily("Runda Light");
-            StatusTextBlock.Margin = new Thickness(75, 65, 0, 0);
             StatusTextBlock.Uid = "StatusTextBlock";
+            StatusTextBlock.Margin = new Thickness(75, 65, 0, 0);
             StatusPopupCanvas.Children.Add(StatusTextBlock);
+            #endregion
 
+            #region CompletedButton
             Button CompletedButton = new Button();
             CompletedButton.Content = new Image
             {
@@ -121,7 +145,9 @@ namespace Health_Tracker
             CompletedButton.BorderThickness = new Thickness(0);
             CompletedButton.Margin = new Thickness(0, 120, 0, 0);
             StatusPopupCanvas.Children.Add(CompletedButton);
+            #endregion
 
+            #region ChangeAchieveByButton
             Button ChangeAchieveByButton = new Button();
             ChangeAchieveByButton.Content = new Image
             {
@@ -134,7 +160,9 @@ namespace Health_Tracker
             ChangeAchieveByButton.BorderThickness = new Thickness(0);
             ChangeAchieveByButton.Margin = new Thickness(90, 120, 0, 0);
             StatusPopupCanvas.Children.Add(ChangeAchieveByButton);
+            #endregion
 
+            #region DeleteGoalButton
             Button DeleteGoalButton = new Button();
             DeleteGoalButton.Content = new Image
             {
@@ -147,7 +175,9 @@ namespace Health_Tracker
             DeleteGoalButton.BorderThickness = new Thickness(0);
             DeleteGoalButton.Margin = new Thickness(260, 120, 0, 0);
             StatusPopupCanvas.Children.Add(DeleteGoalButton);
+            #endregion
 
+            #region ExitButton
             Button ExitButton = new Button();
             ExitButton.Content = new Image
             {
@@ -160,14 +190,19 @@ namespace Health_Tracker
             ExitButton.BorderThickness = new Thickness(0);
             ExitButton.Margin = new Thickness(285, 0, 0, 0);
             StatusPopupCanvas.Children.Add(ExitButton);
+            #endregion
 
             #endregion
+
             StatusPopup.Child = StatusPopupCanvas;
             StatusPopup.IsOpen = true;
+            #endregion
+
         }
 
         private void CompletedButtonClick(object sender, RoutedEventArgs e)
         {
+            #region GetGoalIndex
             int GoalInfoTextBlockIndex = 0;
             foreach (UIElement child in StatusPopupCanvas.Children)
             {
@@ -186,13 +221,21 @@ namespace Health_Tracker
                     GoalIndex = CommonElements.AddedGoalsView.IndexOf(goal);
                 }
             }
+            #endregion
+
+            #region AddGoal
 
             Goal GoalToAdd = CommonElements.AddedGoalsView[GoalIndex];
             CommonElements.CompletedGoalsView.Add(GoalToAdd);
 
+            #endregion
+
+            #region RemoveCompletedGoals
             CommonElements.AddedGoalsView.Remove(CommonElements.AddedGoalsView.Where(i => i.GoalInfo == GoalInfo).Single());
             StatusPopupCanvas.Children.Clear();
             StatusPopup.IsOpen = false;
+            #endregion
+
         }
 
         private void ChangeAchieveByButtonClick(object sender, RoutedEventArgs e)
@@ -230,6 +273,7 @@ namespace Health_Tracker
 
         private void ChangeAchieveByCalendarClick(object sender, RoutedEventArgs e)
         {
+            #region GetGoal
             int GoalInfoTextBlockIndex = 0;
             foreach (UIElement child in StatusPopupCanvas.Children)
             {
@@ -242,7 +286,9 @@ namespace Health_Tracker
             string GoalInfo = ((TextBlock)StatusPopupCanvas.Children[GoalInfoTextBlockIndex]).Text.ToString();
             int GoalIndex = CommonElements.AddedGoalsView.IndexOf(CommonElements.AddedGoalsView.Where(i => i.GoalInfo == GoalInfo).FirstOrDefault());
             CommonElements.AddedGoalsView[GoalIndex].AchieveBy = sender.ToString().Substring(0, 10);
+            #endregion
 
+            #region Close Calendar
             int ChangeAchieveByCalendarIndex = 0;
             foreach(UIElement child in StatusPopupCanvas.Children)
             {
@@ -252,6 +298,9 @@ namespace Health_Tracker
                 }
             }
             StatusPopupCanvas.Children.Remove(StatusPopupCanvas.Children[ChangeAchieveByCalendarIndex]);
+            #endregion
+
+            #region ChangeAchieveByDate
 
             int AchieveByTextBlockIndex = 0;
             foreach(UIElement child in StatusPopupCanvas.Children)
@@ -268,7 +317,9 @@ namespace Health_Tracker
             AchieveByTextBlock.Uid = "AchieveByTextBlock";
             AchieveByTextBlock.Margin = new Thickness(75, 35, 0, 0);
             StatusPopupCanvas.Children.Insert(AchieveByTextBlockIndex, AchieveByTextBlock);
+            #endregion
 
+            #region ChangeStatus
             int StatusTextBlockIndex = 0;
             foreach (UIElement child in StatusPopupCanvas.Children)
             {
@@ -287,6 +338,7 @@ namespace Health_Tracker
             StatusPopupCanvas.Children.Insert(StatusTextBlockIndex, StatusTextBlock);
                 
             StatusPopupCanvas.Background = (Brush)FormatStatusPopup(NewAchieveByDate)[1];
+            #endregion
 
             GoalsTable.Items.Refresh();
         }
