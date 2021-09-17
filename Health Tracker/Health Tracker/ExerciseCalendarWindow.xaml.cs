45bb937a75561a1f845e66598b1d03e6366bae12
anushanarayan2911 <anusha.narayan2002@outlook.com>
2021-09-17 08:32:57 +0100

    Clean up C# code for Exercise Calendar Window

--- Health Tracker/Health Tracker/ExerciseCalendarWindow.xaml.cs
+++ Health Tracker/Health Tracker/ExerciseCalendarWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Health_Tracker
     public partial class ExerciseCalendarWindow : Window
     {
         #region DateVariables
-        IDictionary<string, int> dayNumbers = new Dictionary<string, int>()
+        IDictionary<string, int> DayNumbers = new Dictionary<string, int>()
         {
             {"Monday", 0 },
             {"Tuesday", 1 },
@@ -32,7 +32,7 @@ namespace Health_Tracker
             {"Saturday", 5 }
             , {"Sunday", 6 } 
         };
-        IDictionary<int, string> monthNumbers = new Dictionary<int, string>()
+        IDictionary<int, string> MonthNumbers = new Dictionary<int, string>()
         {
             {1, "January" },
             {2, "February" },
@@ -47,7 +47,7 @@ namespace Health_Tracker
             {11 , "November" },
             {12 , "December" }
         };
-        IDictionary<string, int> daysInMonth = new Dictionary<string, int>()
+        IDictionary<string, int> DaysInMonth = new Dictionary<string, int>()
         {
             {"January", 31 },
             {"February", 28 },
@@ -63,37 +63,42 @@ namespace Health_Tracker
             {"December", 31 }
         };
 
-        int todayDate;
-        string currentMonthNumber;
-        int currentMonthInt;
-        string currentMonth;
+        int TodayDate;
+        string CurrentMonthNumber;
+        int CurrentMonthInt;
+        string CurrentMonth;
 
-        DateTime firstOfMonth;
-        string dayOfFirst;
-        int dayOfFirstNumber;
-        int numberOfDays;
+        DateTime FirstOfMonth;
+        string DayOfFirst;
+        int DayOfFirstNumber;
+        int NumberOfDays;
 
-        Button[] calendarButtonArray = new Button[42];
+        Button[] CalendarButtonArray = new Button[42];
         #endregion
-        ObservableCollection<Exercise> toShow = new ObservableCollection<Exercise>();
+
+        ObservableCollection<Exercise> ToShow = new ObservableCollection<Exercise>();
         Canvas PopupCanvas = new Canvas();
         public ExerciseCalendarWindow()
         {
             InitializeComponent();
 
             #region AddDates
-            todayDate = Int32.Parse(DateTime.Now.ToString().Substring(0, 2));
 
-            currentMonthNumber = DateTime.Now.Month.ToString();
-            currentMonthInt = Int32.Parse(currentMonthNumber);
-            currentMonth = monthNumbers[currentMonthInt].ToString();
-            MonthName.Content = currentMonth;
+            #region StartVariables
+            TodayDate = Int32.Parse(DateTime.Now.ToString().Substring(0, 2));
 
-            firstOfMonth = new DateTime(2021, Int32.Parse(currentMonthNumber), 1);
-            dayOfFirst = firstOfMonth.DayOfWeek.ToString();
-            dayOfFirstNumber = dayNumbers[dayOfFirst];
-            numberOfDays = daysInMonth[currentMonth];
+            CurrentMonthNumber = DateTime.Now.Month.ToString();
+            CurrentMonthInt = Int32.Parse(CurrentMonthNumber);
+            CurrentMonth = MonthNumbers[CurrentMonthInt].ToString();
+            MonthName.Content = CurrentMonth;
 
+            FirstOfMonth = new DateTime(2021, Int32.Parse(CurrentMonthNumber), 1);
+            DayOfFirst = FirstOfMonth.DayOfWeek.ToString();
+            DayOfFirstNumber = DayNumbers[DayOfFirst];
+            NumberOfDays = DaysInMonth[CurrentMonth];
+            #endregion
+
+            #region AddButtons
             for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 7; j++)
@@ -112,48 +117,49 @@ namespace Health_Tracker
 
             foreach (Button button in CalendarGrid.Children)
             {
-                calendarButtonArray[Array.IndexOf(calendarButtonArray, null)] = button;
+                CalendarButtonArray[Array.IndexOf(CalendarButtonArray, null)] = button;
             }
+            #endregion
 
             #region PreviousMonth
-            int previousMonthInt = currentMonthInt - 1;
-            string previousMonth = monthNumbers[previousMonthInt];
-            int daysInPreviousMonth = daysInMonth[previousMonth];
-            int previousMonthStartDate = daysInPreviousMonth - dayOfFirstNumber + 1;
-            int dateToAddPreviousMonth = previousMonthStartDate;
+            int PreviousMonthInt = CurrentMonthInt - 1;
+            string PreviousMonth = MonthNumbers[PreviousMonthInt];
+            int DaysInPreviousMonth = DaysInMonth[PreviousMonth];
+            int PreviousMonthStartDate = DaysInPreviousMonth - DayOfFirstNumber + 1;
+            int DateToAddPreviousMonth = PreviousMonthStartDate;
 
-            for (int i = 0; i < dayOfFirstNumber; i++)
+            for (int i = 0; i < DayOfFirstNumber; i++)
             {
-                calendarButtonArray[i].Content = dateToAddPreviousMonth.ToString();
-                dateToAddPreviousMonth += 1;
-                calendarButtonArray[i].Opacity = 0.5;
+                CalendarButtonArray[i].Content = DateToAddPreviousMonth.ToString();
+                DateToAddPreviousMonth += 1;
+                CalendarButtonArray[i].Opacity = 0.5;
             }
             #endregion
 
             #region CurrentMonth
-            int dateToAddCurrentMonth = 1;
-            for (int i = dayOfFirstNumber; i < dayOfFirstNumber + numberOfDays; i++)
+            int DateToAddCurrentMonth = 1;
+            for (int i = DayOfFirstNumber; i < DayOfFirstNumber + NumberOfDays; i++)
             {
-                calendarButtonArray[i].Content = dateToAddCurrentMonth.ToString();
+                CalendarButtonArray[i].Content = DateToAddCurrentMonth.ToString();
                
-                if(dateToAddCurrentMonth == todayDate)
+                if(DateToAddCurrentMonth == TodayDate)
                 {
-                    calendarButtonArray[i].Background = new SolidColorBrush(Color.FromRgb(25, 149, 159));
-                    calendarButtonArray[i].Background.Opacity = 0.5;
+                    CalendarButtonArray[i].Background = new SolidColorBrush(Color.FromRgb(25, 149, 159));
+                    CalendarButtonArray[i].Background.Opacity = 0.5;
                 }
-                dateToAddCurrentMonth += 1;
+                DateToAddCurrentMonth += 1;
             };
             #endregion
 
             #region NextMonth
-            int nextMonthInt = currentMonthInt + 1;
-            int dateToAddNextMonth = 1;
+            int NextMonthInt = CurrentMonthInt + 1;
+            int DateToAddNextMonth = 1;
 
-            for (int i = dayOfFirstNumber + numberOfDays; i < 42; i++)
+            for (int i = DayOfFirstNumber + NumberOfDays; i < 42; i++)
             {
-                calendarButtonArray[i].Content = dateToAddNextMonth.ToString();
-                dateToAddNextMonth += 1;
-                calendarButtonArray[i].Opacity = 0.5;
+                CalendarButtonArray[i].Content = DateToAddNextMonth.ToString();
+                DateToAddNextMonth += 1;
+                CalendarButtonArray[i].Opacity = 0.5;
             }
             #endregion
 
@@ -170,72 +176,85 @@ namespace Health_Tracker
         protected void CalendarButtonClick(object sender, RoutedEventArgs e)
         {
 
-            int row = Grid.GetRow((Button) sender);
-            int col = Grid.GetColumn((Button)sender);
+            #region GetWhichButtonWasClicked
+            int Row = Grid.GetRow((Button) sender);
+            int Col = Grid.GetColumn((Button)sender);
 
-            var dayPair = dayNumbers.FirstOrDefault(x => x.Value == col);
-            string day = String.Join(",", dayPair);
-            int index = day.IndexOf(",");
-            day = day.Substring(1, index - 1);
+            var DayPair = DayNumbers.FirstOrDefault(x => x.Value == Col);
+            string Day = String.Join(",", DayPair);
+            int index = Day.IndexOf(",");
+            Day = Day.Substring(1, index - 1);
 
-            foreach(Exercise exercise in CommonElements.AddedExercisesView)
+            #endregion
+
+            #region GetWhichExercisesToShow
+            foreach (Exercise exercise in CommonElements.AddedExercisesView)
             {
-                if (exercise.exerciseFrequency.Contains(day) | exercise.exerciseFrequency == "daily")
+                if (exercise.exerciseFrequency.Contains(Day) | exercise.exerciseFrequency == "daily")
                 {
-                    DateTime exerciseStartDate = Convert.ToDateTime(exercise.exerciseDate.Substring(0, 10));
-                    DateTime exerciseEndDate = Convert.ToDateTime(exercise.exerciseDate.Substring(13, 10));
+                    DateTime ExerciseStartDate = Convert.ToDateTime(exercise.exerciseDate.Substring(0, 10));
+                    DateTime ExerciseEndDate = Convert.ToDateTime(exercise.exerciseDate.Substring(13, 10));
 
-                    DateTime cellDate = Convert.ToDateTime(((Button)sender).Content.ToString() + "/" + currentMonthNumber + "/" + "2021");
+                    DateTime CellDate = Convert.ToDateTime(((Button)sender).Content.ToString() + "/" + CurrentMonthNumber + "/" + "2021");
                     
-                    if (exerciseStartDate <= cellDate & cellDate <= exerciseEndDate)
+                    if (ExerciseStartDate <= CellDate & CellDate <= ExerciseEndDate)
                     {
-                        if (!toShow.Contains(exercise))
+                        if (!ToShow.Contains(exercise))
                         {
-                            toShow.Add(exercise);
+                            ToShow.Add(exercise);
                         }  
                     }
                 }
             }
+            #endregion
 
-            ExerciseDetailsPopup.Height = toShow.Count * 100;
-
+            #region ExercisePopup
+            ExerciseDetailsPopup.Height = ToShow.Count * 100;
 
             PopupCanvas.Background = new SolidColorBrush(Color.FromRgb(25, 149, 159));
 
-            for (int i = 0; i < toShow.Count; i++)
+            for (int i = 0; i < ToShow.Count; i++)
             {
+                #region AddExerciseElementsToPopup
                 TextBlock CurrentExerciseTitle = new TextBlock();
-                CurrentExerciseTitle.Text = toShow[i].exerciseName;
+                CurrentExerciseTitle.Text = ToShow[i].exerciseName;
                 CurrentExerciseTitle.TextDecorations = TextDecorations.Underline;
                 CurrentExerciseTitle.FontFamily = new FontFamily("Runda");
+                CurrentExerciseTitle.Uid = "CurrentExerciseTitle";
                 CurrentExerciseTitle.Margin = new Thickness(100, i * 100, 0, 0);
                 PopupCanvas.Children.Add(CurrentExerciseTitle);
 
                 Label CurrentExerciseDetailsLabel = new Label();
                 CurrentExerciseDetailsLabel.Content = "Details:";
-                CurrentExerciseTitle.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseDetailsLabel.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseDetailsLabel.Uid = "CurrentExerciseDetailsLabel";
                 CurrentExerciseDetailsLabel.Margin = new Thickness(10, i * 100 + 20, 0, 0);
                 PopupCanvas.Children.Add(CurrentExerciseDetailsLabel);
 
                 TextBlock CurrentExerciseDetails = new TextBlock();
-                CurrentExerciseDetails.Text = toShow[i].exerciseDetails;
-                CurrentExerciseTitle.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseDetails.Text = ToShow[i].exerciseDetails;
+                CurrentExerciseDetails.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseDetails.Uid = "CurrentExerciseDetails";
                 CurrentExerciseDetails.Margin = new Thickness(65, i * 100 + 25, 0, 0);
                 PopupCanvas.Children.Add(CurrentExerciseDetails);
 
                 Label CurrentExerciseTimeLabel = new Label();
                 CurrentExerciseTimeLabel.Content = "Time:";
-                CurrentExerciseTitle.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseTimeLabel.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseTimeLabel.Uid = "CurrentExerciseTimeLabel";
                 CurrentExerciseTimeLabel.Margin = new Thickness(10, i * 100 + 40, 0, 0);
                 PopupCanvas.Children.Add(CurrentExerciseTimeLabel);
 
                 TextBlock CurrentExerciseTime = new TextBlock();
-                CurrentExerciseTime.Text = toShow[i].exerciseTime;
-                CurrentExerciseTitle.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseTime.Text = ToShow[i].exerciseTime;
+                CurrentExerciseTime.FontFamily = new FontFamily("Runda Light");
+                CurrentExerciseTime.Uid = "CurrentExerciseTime";
                 CurrentExerciseTime.Margin = new Thickness(65, i * 100 + 45, 0, 0);
                 PopupCanvas.Children.Add(CurrentExerciseTime);
+                #endregion
             }
 
+            #region AddQuitButtonToPopup
             Button QuitButton = new Button();
             QuitButton.Content = new Image {
                 Source = new BitmapImage(new Uri("C:/Users/anush/source/repos/health-tracker/Images/Close.png"))
@@ -246,16 +265,18 @@ namespace Health_Tracker
             QuitButton.BorderThickness = new Thickness(0);
             QuitButton.Margin = new Thickness(190, 0, 0, 0);
             PopupCanvas.Children.Add(QuitButton);
+            #endregion
 
             ExerciseDetailsPopup.Child = PopupCanvas;
             ExerciseDetailsPopup.IsOpen = true;
+            #endregion
         }
 
         public void QuitButtonClick(object sender, RoutedEventArgs e)
         {
             ExerciseDetailsPopup.IsOpen = false;
             PopupCanvas.Children.Clear();
-            toShow.Clear();
+            ToShow.Clear();
         }
 
         private void OpenFoodWindow(object sender, RoutedEventArgs e)
